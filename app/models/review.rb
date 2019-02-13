3ebfb1aa2599e0e5e33b1f4c34b2ea1bba40b941
Amaury Chaboche <amaury.chaboche@gmail.com>
2019-02-13 15:28:30 +0100

    exo done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,11 @@
 class Review < ApplicationRecord
+  GRADES = [0, 1, 2, 3, 4, 5]
   belongs_to :restaurant
   validates :content, presence: true
   validates :restaurant_id, presence: true
-  validates :rating, inclusion: { in: [0,1, 2, 3, 4, 5] }
+  validates :rating, inclusion: { in: GRADES }
   validates :rating, numericality: { only_integer: true }
 end
+
+
+
